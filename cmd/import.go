cb177bc38e4f2d19dca86d45c5ff245a00f51b7e
DarkMatterMatt <mattmoran2000@gmail.com>
2020-02-01 21:34:51 +1300

    Customise cobra command template

--- cmd/import.go
+++ cmd/import.go
@@ -9,17 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// importCmd represents the import command
+// the `import` command
 var importCmd = &cobra.Command{
 	Use:   "import",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: runImport,
+	Short: "Import a file or folder into a database.",
+	Long:  "",
+	Run:   runImport,
 }
 
 func init() {
--- cmd/search.go
+++ cmd/search.go
@@ -9,17 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// searchCmd represents the search command
+// the `search` command
 var searchCmd = &cobra.Command{
 	Use:   "search",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: runSearch,
+	Short: "Search multiple databases simultaneously.",
+	Long:  "",
+	Run:   runSearch,
 }
 
 func init() {
