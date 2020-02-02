0e1d9c44c98d374e40e3f4ec5a4e22048baeee76
DarkMatterMatt <mattmoran2000@gmail.com>
2020-02-02 22:00:23 +1300

    Add UTF-8 string Reverse function

---
+++ pkg/reverse/reverse.go
@@ -0,0 +1,12 @@
+package reverse
+
+// Reverse creates a reversed copy of a UTF-8 string
+func Reverse(s string) string {
+	n := len(s)
+	runes := make([]rune, n)
+	for _, rune := range s {
+		n--
+		runes[n] = rune
+	}
+	return string(runes[n:])
+}
