11524cf223585ed4a46cb6a6b2d44f8636786005
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-02-11 15:24:18 +1300

    Add search functionality

--- cmd/search.go
+++ cmd/search.go
@@ -5,9 +5,17 @@ package cmd
  */
 
 import (
-	"github.com/spf13/cobra"
+	"database/sql"
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
 
+	"github.com/darkmattermatt/DumpDB/internal/sourceid"
+	"github.com/darkmattermatt/DumpDB/pkg/stringinslice"
 	l "github.com/darkmattermatt/dumpdb/pkg/simplelog"
+	"github.com/spf13/cobra"
 )
 
 // the `search` command
@@ -38,6 +46,114 @@ func init() {
 	v.BindPFlags(searchCmd.Flags())
 }
 
+func loadSearchConfig() error {
+	c.Databases = strings.Split(v.GetString("db"), ",")
+	c.ConnPrefix = v.GetString("connPrefix")
+	c.DbTable = v.GetString("dbTable")
+	c.SourcesConn = v.GetString("sourcesConn")
+	c.SourcesTable = v.GetString("sourcesTable")
+	c.Query = v.GetString("query")
+	c.Columns = strings.Split(strings.ToLower(v.GetString("columns")), ",")
+
+	validCols := []string{"email", "hash", "password", "source", "sourceid", "username"}
+	for _, col := range c.Columns {
+		if !stringinslice.StringInSlice(col, validCols) {
+			return errors.New("Invalid column name: " + col)
+		}
+	}
+	return nil
+}
+
 func runSearch(cmd *cobra.Command, args []string) {
-	l.I("search called")
+	err := loadSearchConfig()
+	l.FatalOnErr(err)
+
+	l.I("Querying", len(c.Databases), "databases:", strings.Join(c.Databases, ", "))
+	l.V("Output format is tab-delimited as:")
+	l.V(strings.Join(c.Columns, "\t"))
+
+	var wg sync.WaitGroup
+	for _, dbName := range c.Databases {
+		wg.Add(1)
+		go queryDatabase(dbName, &wg)
+	}
+	wg.Wait()
+}
+
+// preferUsingEmailRev replaces queries using the `email` column with queries using the `email_rev` column
+func preferUsingEmailRev(stmt string) string {
+	/** Prefer using email_rev column because it is indexed
+	 * - replaces               | + with
+	 * email like '%@gmail.com' | email_rev like REVERSE('%@gmail.com')
+	 * email = "test@gmail.com" | email_rev = REVERSE("test@gmail.com")
+	 * email >= 'abc@gmail.com' | email_rev >= REVERSE('abc@gmail.com')
+	 * email LIKE 'DarkMatter%' | email_rev LIKE REVERSE('DarkMatter%') <- this will _not_ use the email_rev index
+	 */
+	return regexp.MustCompile("(?i)email\\s*(LIKE|[<>!=]{1,2})\\s*('[^']*'|\"[^\"]*\")").ReplaceAllString(stmt, "email_rev $1 REVERSE($2)")
+}
+
+func queryDatabase(dbName string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	dbConn := c.ConnPrefix + dbName
+	l.D("queryDatabase", "dbConn:", dbConn)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		l.W(err)
+		return
+	}
+	defer db.Close()
+
+	q := "SELECT email, hash, password, sourceid, username FROM main WHERE " + c.Query
+	l.D("queryDatabase", dbName, q)
+
+	rows, err := db.Query(q)
+	if err != nil {
+		l.W(err)
+		return
+	}
+	defer rows.Close()
+
+	var (
+		email    string
+		hash     string
+		password string
+		sourceID uint32
+		username string
+	)
+
+	for rows.Next() {
+		err := rows.Scan(&email, &hash, &password, &sourceID, &username)
+		if err != nil {
+			l.W(err)
+			return
+		}
+
+		var b strings.Builder
+		for _, col := range c.Columns {
+			switch col {
+			case "email":
+				b.WriteString(email)
+			case "hash":
+				b.WriteString(hash)
+			case "password":
+				b.WriteString(password)
+			case "source":
+				b.WriteString(sourceid.Source(sourceID))
+			case "sourceID":
+				b.WriteString(strconv.FormatUint(uint64(sourceID), 10))
+			case "username":
+				b.WriteString(username)
+			}
+			b.WriteString("\t")
+		}
+		// trim trailing tab, then print
+		l.R(b.String()[:b.Len()-1])
+	}
+
+	err = rows.Err()
+	if err != nil {
+		l.W(err)
+		return
+	}
 }
