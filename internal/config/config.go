e0d87f8da8a952fbfdae12bf5df16d6eef7db3ed
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-02-11 15:15:28 +1300

    Create config.go

---
+++ internal/config/config.go
@@ -0,0 +1,16 @@
+package config
+
+// Config contains the configuration options for DumpDB
+type Config struct {
+	Verbosity  int
+	ConfigFile string
+
+	Databases     []string
+	ConnPrefix    string
+	DbTable       string
+	SourcesConn   string
+	SourcesTable  string
+	Query         string
+	Columns       []string
+	ResolveSource bool
+}
