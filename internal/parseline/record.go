c0324c85f4e581a1e6fc0049272c5c6d23b9c507
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-02-12 20:11:05 +1300

    Update SourceID type to match sourceid package

--- internal/parseline/record.go
+++ internal/parseline/record.go
@@ -3,7 +3,7 @@ package parseline
 // A Record represents a single row in the database
 type Record struct {
 	Source   string
-	SourceID uint32
+	SourceID int64
 	Username string
 	Email    string
 	EmailRev string
