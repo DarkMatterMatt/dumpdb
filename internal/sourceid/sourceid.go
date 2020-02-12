b1c26995f2823cd1ab23bd5dc9301349d844fb44
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-02-12 21:55:20 +1300

    Rename source* to sources*

--- internal/sourceid/sourceid.go
+++ internal/sourceid/sourceid.go
@@ -14,7 +14,7 @@ var (
 )
 
 // SourceName resolves a source ID to it's source name
-func SourceName(id int64, sourceDb *sql.DB, sourceTable string) (string, error) {
+func SourceName(id int64, sourcesDb *sql.DB, sourcesTable string) (string, error) {
 	// load from cache
 	val, ok := sourceNameCache.Get(id)
 	if ok {
@@ -26,9 +26,9 @@ func SourceName(id int64, sourceDb *sql.DB, sourceTable string) (string, error) 
 
 	// fetch from database
 	var s string
-	err := sourceDb.QueryRow(`
+	err := sourcesDb.QueryRow(`
 		SELECT name
-		FROM `+sourceTable+`
+		FROM `+sourcesTable+`
 		WHERE id=?
 	`, id).Scan(&s)
 	if err != nil {
@@ -44,7 +44,7 @@ func SourceName(id int64, sourceDb *sql.DB, sourceTable string) (string, error) 
 }
 
 // SourceID fetches an integer ID for a string `s` from the sources table
-func SourceID(s string, sourceDb *sql.DB, sourceTable string) (int64, error) {
+func SourceID(s string, sourcesDb *sql.DB, sourcesTable string) (int64, error) {
 	if strings.HasPrefix(s, "./") {
 		s = s[2:]
 	}
@@ -64,8 +64,8 @@ func SourceID(s string, sourceDb *sql.DB, sourceTable string) (int64, error) {
 	}
 
 	// upsert from database
-	res, err := sourceDb.Exec(`
-		INSERT INTO `+sourceTable+` (name, date_added) VALUES(?, UNIX_TIMESTAMP())
+	res, err := sourcesDb.Exec(`
+		INSERT INTO `+sourcesTable+` (name, date_added) VALUES(?, UNIX_TIMESTAMP())
 		ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id)
 	`, s)
 	if err != nil {
