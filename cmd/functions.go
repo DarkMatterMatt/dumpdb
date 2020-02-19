30edb33db44007d569498624bbaefb324f24e786
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-02-19 21:21:39 +1300

    Use myisampack if using the MyISAM engine

--- cmd/functions.go
+++ cmd/functions.go
@@ -30,7 +30,13 @@ func getDataDir() (dataDir string) {
 
 func disableDatabaseIndexes(dataDir string) {
 	l.V("Disabling database indexes")
-	out, err := exec.Command("aria_chk", "-rq", "--keys-used", "0", dataDir+c.Database+"/"+mainTable).CombinedOutput()
+
+	packCmd := "aria_chk"
+	if c.Engine == "myisam" {
+		packCmd = "myisamchk"
+	}
+
+	out, err := exec.Command(packCmd, "-rq", "--keys-used", "0", dataDir+c.Database+"/"+mainTable).CombinedOutput()
 	l.FatalOnErr(err)
 	l.D(out)
 }
@@ -40,6 +46,7 @@ func restoreDatabaseIndexes(dataDir, tmpDir string) {
 
 	mem := memory.TotalMemory()
 	if mem != 0 {
+		// TODO: Add configurable percentage
 		l.V("Detected RAM: " + strconv.FormatUint(mem/1024/1024/1000, 10) + "GB. Using 25% as the sort buffer.")
 		mem /= 4
 	} else {
@@ -47,14 +54,27 @@ func restoreDatabaseIndexes(dataDir, tmpDir string) {
 		mem = 512 * 1024 * 1024
 	}
 
-	out, err := exec.Command("aria_pack", "--aria_sort_buffer_size", strconv.FormatUint(mem, 10), "--tmpdir", tmpDir, dataDir+c.Database+"/"+mainTable).CombinedOutput()
+	packCmd := "aria_chk"
+	bufferParam := "--aria_sort_buffer_size"
+	if c.Engine == "myisam" {
+		packCmd = "myisamchk"
+		bufferParam = "--myisam_sort_buffer_size"
+	}
+
+	out, err := exec.Command(packCmd, bufferParam, strconv.FormatUint(mem, 10), "--tmpdir", tmpDir, dataDir+c.Database+"/"+mainTable).CombinedOutput()
 	l.FatalOnErr(err)
 	l.D(out)
 }
 
 func compressDatabase(dataDir, tmpDir string) {
 	l.V("Compressing database")
-	out, err := exec.Command("aria_chk", "-rq", "--tmpdir", tmpDir, dataDir+c.Database+"/"+mainTable).CombinedOutput()
+
+	packCmd := "aria_pack"
+	if c.Engine == "myisam" {
+		packCmd = "myisampack"
+	}
+
+	out, err := exec.Command(packCmd, "-rq", "--tmpdir", tmpDir, dataDir+c.Database+"/"+mainTable).CombinedOutput()
 	l.FatalOnErr(err)
 	l.D(out)
 }
