750f9d9b744e5d5034d7465d74bd7c03651f0847
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-02-26 13:46:53 +1300

    Expose internal functions

--- pkg/simplelog/simplelog.go
+++ pkg/simplelog/simplelog.go
@@ -17,22 +17,35 @@ const (
 )
 
 var (
-	getVerbosity  func() int
-	loggerFatal   = log.New(os.Stderr, "[FATAL] ", 0)
-	loggerResult  = log.New(os.Stdout, "", 0)
-	loggerWarning = log.New(os.Stderr, "[!] ", 0)
-	loggerInfo    = log.New(os.Stdout, "[+] ", 0)
-	loggerVerbose = log.New(os.Stdout, "[>] ", 0)
-	loggerDebug   = log.New(os.Stdout, "[D] ", 0)
+	// GetVerbosity returns the current verbosity level
+	GetVerbosity func() int
+
+	// LoggerFatal is written to for FATAL level logging
+	LoggerFatal = log.New(os.Stderr, "[FATAL] ", 0)
+
+	// LoggerResult is written to for RESULT level logging
+	LoggerResult = log.New(os.Stdout, "", 0)
+
+	// LoggerWarning is written to for WARNING level logging
+	LoggerWarning = log.New(os.Stderr, "[!] ", 0)
+
+	// LoggerInfo is written to for INFO level logging
+	LoggerInfo = log.New(os.Stdout, "[+] ", 0)
+
+	// LoggerVerbose is written to for VERBOSE level logging
+	LoggerVerbose = log.New(os.Stdout, "[>] ", 0)
+
+	// LoggerDebug is written to for DEBUG level logging
+	LoggerDebug = log.New(os.Stdout, "[D] ", 0)
 )
 
 // GetVerbosityWith sets the function used to load the current verbosity level
 func GetVerbosityWith(v func() int) {
-	getVerbosity = v
+	GetVerbosity = v
 }
 
 func assertInitialized() {
-	if getVerbosity == nil {
+	if GetVerbosity == nil {
 		panic("Logger is not initialized!")
 	}
 }
@@ -40,48 +53,48 @@ func assertInitialized() {
 // F logs FATAL errors to stderr and calls os.Exit(1)
 func F(v ...interface{}) {
 	assertInitialized()
-	if getVerbosity() >= FATAL {
-		loggerFatal.Fatalln(v...)
+	if GetVerbosity() >= FATAL {
+		LoggerFatal.Fatalln(v...)
 	}
 }
 
 // R logs RESULTS to stdout
 func R(v ...interface{}) {
 	assertInitialized()
-	if getVerbosity() >= RESULT {
-		loggerResult.Println(v...)
+	if GetVerbosity() >= RESULT {
+		LoggerResult.Println(v...)
 	}
 }
 
 // W logs WARNINGS to stderr
 func W(v ...interface{}) {
 	assertInitialized()
-	if getVerbosity() >= WARNING {
-		loggerWarning.Println(v...)
+	if GetVerbosity() >= WARNING {
+		LoggerWarning.Println(v...)
 	}
 }
 
 // I logs INFO to stdout
 func I(v ...interface{}) {
 	assertInitialized()
-	if getVerbosity() >= INFO {
-		loggerInfo.Println(v...)
+	if GetVerbosity() >= INFO {
+		LoggerInfo.Println(v...)
 	}
 }
 
 // V logs VERBOSE messages to stdout
 func V(v ...interface{}) {
 	assertInitialized()
-	if getVerbosity() >= VERBOSE {
-		loggerVerbose.Println(v...)
+	if GetVerbosity() >= VERBOSE {
+		LoggerVerbose.Println(v...)
 	}
 }
 
 // D logs DEBUG messages to stdout
 func D(v ...interface{}) {
 	assertInitialized()
-	if getVerbosity() >= DEBUG {
-		loggerDebug.Println(v...)
+	if GetVerbosity() >= DEBUG {
+		LoggerDebug.Println(v...)
 	}
 }
 
